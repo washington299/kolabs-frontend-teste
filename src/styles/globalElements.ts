f79a9155db33210f1d0d60773ad8d51cd8f16f58
Washington Campos <washingtoncampos9@gmail.com>
2021-09-14 13:52:05 -0300

    feat: create Container global element

---
+++ src/styles/globalElements.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+	max-width: 1180px;
+	margin: auto;
+
+	@media screen and (max-width: 1180px) {
+		padding: 0 var(--small-size);
+	}
+`;
