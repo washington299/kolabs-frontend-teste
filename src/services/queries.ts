914dd61a025867553fd4171eab83730c574549fd
Washington Campos <washingtoncampos9@gmail.com>
2021-09-16 15:50:05 -0300

    feat: add page query parameter

--- src/services/queries.ts
+++ src/services/queries.ts
@@ -1,6 +1,8 @@
 import { api } from "services/config";
 
 export const queries = {
-	getAllResults: (category: string | string[], word: string | string[]) =>
-		api.get(`/search/${category}?api_key=${process.env.TDBM_API_KEY}&query=${word}&language=pt`),
+	getAllResults: (category: string | string[], word: string | string[], page: string | string[]) =>
+		api.get(
+			`/search/${category}?api_key=${process.env.TDBM_API_KEY}&query=${word}&language=pt&page=${page}`,
+		),
 };
