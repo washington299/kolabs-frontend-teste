d91afdfba1ed2e6b50aa90c29087d41d7e28678e
Washington Campos <washingtoncampos9@gmail.com>
2021-09-18 19:44:51 -0300

    feat: make api request when search for a movie

--- src/components/SearchField/index.tsx
+++ src/components/SearchField/index.tsx
@@ -1,12 +1,24 @@
+import { useRouter } from "next/router";
+import { useState } from "react";
 import { Search } from "styled-icons/bootstrap";
 
 import * as S from "./styles";
 
 export const SearchField = () => {
+	const [value, setValue] = useState("");
+
+	const { push } = useRouter();
+
+	const handleClick = () => {
+		if (!value) return;
+
+		push(`?category=multi&search=${value}`);
+	};
+
 	return (
 		<S.InputWrapper>
-			<Search size={20} style={{ cursor: "pointer" }} />
-			<S.Input placeholder="avengers" />
+			<Search size={20} style={{ cursor: "pointer" }} onClick={handleClick} />
+			<S.Input placeholder="avengers" value={value} onChange={e => setValue(e.target.value)} />
 		</S.InputWrapper>
 	);
 };
